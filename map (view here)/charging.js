ae7ba84be356dc602a80801b95b293ee7f9e7769
yut4-hub <63132891+yut4-hub@users.noreply.github.com>
2021-08-27 20:16:28 -0500

    initial commit

---
+++ map (view here)/charging.js
@@ -0,0 +1,19 @@
+data=
+[
+[	40.76287079	,	-111.8897629	,	1	,	"	55 East 300 South	"	]	,
+[	40.7659093	,	-111.890985	,	1	,	"	159 Main Street	"	]	,
+[	40.72263336	,	-111.8620911	,	1	,	"	Fairmont Aquatic Center – 1044 E. Sugarmont Dr	"	]	,
+[	40.71583176	,	-111.8654327	,	1	,	"	Forest Dale Golf Course – 2375 South 900 East	"	]	,
+[	40.746319	,	-111.916924	,	1	,	"	International Peace Gardens – 1060 S 900 W	"	]	,
+[	40.74978256	,	-111.8739014	,	1	,	"	Liberty Park (western loop) – 600 East 900 South	"	]	,
+[	40.7595925	,	-111.8837747	,	1	,	"	Library Square Garage (P1) – 210 East 400 South	"	]	,
+[	40.76055908	,	-111.881897	,	1	,	"	Library Square Garage (P2) – 210 East 400 South	"	]	,
+[	40.758398	,	-111.8837046	,	1	,	"	On-Street Parking, South of The Leonardo – 255 East 500 South 	"	]	,
+[	40.7624161	,	-111.922435	,	1	,	"	Pioneer Park* – 349 West 300 South	"	]	,
+[	40.8162997	,	-111.9482543	,	1	,	"	Regional Athletic Complex** – 2350 Rose Park Ln	"	]	,
+[	40.74227524	,	-111.9850464	,	1	,	"	Sorenson Multi-Cultural Center – 855 W California Ave	"	]	,
+[	40.7232029	,	-111.857124	,	1	,	"	Sugar House – 1170 Wilmington Ave 	"	]	,
+[	40.75099182	,	-111.8154373	,	1	,	"	Sunnyside near Hogle Zoo – 2600 Sunnyside Ave	"	]	,
+[	40.663886	,	-111.97079	,	1	,	"	Mountain Dell Golf Course – Uinta-Wasatch-Cache Forest 	"	]	,
+
+];                                                
